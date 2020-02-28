e2ff37a31cbc90fc680f601ef3cefca6ab1b2aff
anvar <antnotes7@gmail.com>
2020-02-28 15:14:32 -0500

    add changes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/classes/employee/employee';
+import { EmployeeService } from 'src/app/services/employee/employee.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _employeeService: EmployeeService) {}
+
+  employee = new Employee("", "");
 
   ngOnInit(): void {
+  }
+
+  onLogin() {
+
+    console.log(this.employee);
+
+    this._employeeService.create(this.employee)
+      .subscribe(
+        data => this._employeeService.setCurrentUser(data),
+        error => console.error('Error!', error)
+      )
+    
+    console.log(this._employeeService.getCurrentUser);
+
   }
 
 }
