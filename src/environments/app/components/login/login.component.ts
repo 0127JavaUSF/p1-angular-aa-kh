b277af4a98839f0297d6ef990330188bbe3b381c
anvar <antnotes7@gmail.com>
2020-02-29 15:20:41 -0500

    add Logout

---
+++ src/environments/app/components/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../classes/employee/employee';
+import { EmployeeService } from '../../services/employee/employee.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  flag: boolean;
+
+  constructor(private _employeeService: EmployeeService) {}
+  
+  employee = new Employee("", "");
+
+  ngOnInit(): void {
+  }
+
+  onLogin() {
+
+    console.log(this.employee);
+
+    this._employeeService.create(this.employee)
+      .subscribe(
+        data => {
+          this._employeeService.setCurrentUser(data);
+          if(this._employeeService.getCurrentUser()) {
+            this.flag = true;
+          }
+        },
+          error => console.error('Error!', error)
+      )
+    
+    console.log(this._employeeService.getCurrentUser);
+
+  }
+
+  onLogout() {
+    this._employeeService.destroy()
+      .subscribe(
+        () => console.log("Logged out."),
+        error => console.log('Error!', error)
+      )
+  }
+
+}
